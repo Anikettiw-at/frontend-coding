8f477dfc024a3394314db93ad45a83afaf7cb4eb
Aniket Tiwari <pt5602238@gmail.com>
2026-01-10 21:32:46 +0530

    updated frontend for deploy

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,12 +1,19 @@
 import axios from "axios"
 
+// const axiosClient =  axios.create({
+//     baseURL: 'http://localhost:3000',
+//     withCredentials: true,
+//     // headers: {
+//     //     'Content-Type': 'application/json'
+//     // }
+// });
+
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
-    withCredentials: true,
-    // headers: {
-    //     'Content-Type': 'application/json'
-    // }
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
+
+
 
 
 export default axiosClient;
